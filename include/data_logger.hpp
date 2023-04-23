dfc1b02fe71a12ebc0cec0e41099ee02d5a9ee7e
Jacob Chisholm <Jacobchisholm1010@gmail.com>
2023-04-23 18:12:20 -0400

    initial values

--- include/data_logger.hpp
+++ include/data_logger.hpp
@@ -9,9 +9,9 @@
  * @copyright Copyright (c) 2023
  * 
  */
-//#include "../QFSAE_Library_Dependencies/include/qfsae_structs.hpp"
+#include "../QFSAE_Library_Dependencies/include/qfsae_structs.hpp"
 
-#include "qfsae_structs.hpp"
+//#include "qfsae_structs.hpp"
 
 namespace CAL
 {
@@ -22,9 +22,9 @@ namespace CAL
     namespace DATA_ECU_LOGGER
     {
         /**
-         * @brief Engine Speed
+         * @brief Engine Speed (RPM)
          * Contained as a 16 Bit Integer,
-         * Accessed at: ID 0x0640 | Byte 0
+         * Accessed at: ID 0x0640 | Byte 0/1
          */
         constexpr data EngineSpeed = {
             MOTEC_ID::Logger_1,
@@ -32,6 +32,48 @@ namespace CAL
             0xFFFF,
             1,
             6,
+            DataType::int16
+        };
+        
+        /**
+         * @brief Inlet Manifold Pressure (0.1kPa)
+         * Contained as a 16 Bit Integer,
+         * Accessed at: ID 0x0640 | Byte 2/3
+         */
+        constexpr data IMP = {
+            MOTEC_ID::Logger_1,
+            2,
+            0xFFFF,
+            1,
+            1,
+            DataType::int16
+        };
+
+        /**
+         * @brief Inlet Air Temperature (0.1C)
+         * Contained as a 16 Bit Integer,
+         * Accessed at: ID 0x0640 | Byte 4/5
+         */
+        constexpr data InAirTemp = {
+            MOTEC_ID::Logger_1,
+            4,
+            0xFFFF,
+            1,
+            1,
+            DataType::int16
+        };
+
+        /**
+         * @brief Throttle Position (0.001%)
+         * Contained as a 16 Bit Integer,
+         * Accessed at: ID 0x0640 | Byte 6/7
+         */
+        constexpr data ThrottlePosition = {
+            MOTEC_ID::Logger_1,
+            6,
+            0xFFFF,
+            1,
+            1,
             DataType::int16
         };
 
